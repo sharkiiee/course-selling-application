bab59e23f489b9bd7bb4b83d8b0a77609e3ecb6f
sharkiiee <sarthak231203@gmail.com>
2026-01-04 01:50:36 +0530

    made featurePrograms responsive for the website

--- frontend/src/components/FeaturedPrograms.jsx
+++ frontend/src/components/FeaturedPrograms.jsx
@@ -3,18 +3,36 @@ import Container from "./Container";
 import styles from "./FeaturedPrograms.module.css";
 
 import nodejs from "/nodejs.png";
+import second from "/second.png";
 
 export default function FeaturedPrograms() {
   return (
     <Container style={{ marginTop: "50px" }}>
       <div>
-      <div className={styles.featuredHeading}>
+        
+        <div className={styles.featuredHeading}>
           <h2>Featured Programs</h2>
           <p>Our most comprehensive and impactful learning experiences</p>
         </div>
+
         <div className={styles.coursesGrid}>
           <div className={styles.courseCard}>
             <img src={nodejs} alt="Node.js" />
+            <div className={styles.courseInfo}>
+              <h3>100xSchool Combined Bootcamp</h3>
+              <p>
+                Web dev (Every Friday) Devops (Every Friday) Machine Learning
+                and AI (Every Saturday) Web3 (Every Sunday) DSA (Every Thursday)
+              </p>
+              <h4>
+                ₹5,999{" "}
+                <span>₹8,999</span>
+              </h4>
+              <button>View Details</button>
+            </div>
+          </div>
+          <div className={styles.courseCard}>
+            <img src={second} alt="Node.js" />
             <div className={styles.courseInfo}>
               <h3>100xSchool Combined Bootcamp</h3>
               <p>
@@ -29,6 +47,7 @@ export default function FeaturedPrograms() {
             </div>
           </div>
         </div>
+
       </div>
     </Container>
   );
