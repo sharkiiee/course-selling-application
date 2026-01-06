ac111c997b61964c26c7fbdd14ababff1e5650b2
sharkiiee <sarthak231203@gmail.com>
2026-01-06 17:29:34 +0530

    fixed some issues

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://sarthak231203:mathura234@cluster0.wr1lefw.mongodb.net/CourseManagement");
+mongoose.connect("Your Connection_String_Here");
 
 const AdminSchema = new mongoose.Schema({
     username: String,
